8b6a3bd5ca859a75a82c1bf1bb8f5c27f5767db1
Peter <peter.zgurovets@gmail.com>
2022-04-16 16:10:33 -0700

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,46 @@
+const imgDom = document.querySelectorAll(".container img");
+const heading = document.querySelector(".result");
+  // Game result player1.
+  const scorePlayer1 = [];
+  const scorePlayer2 = [];
+
+//The Function creates and return one random image.
+function randomDiceImage() {
+  var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+  var diceImage = "images/dice" + randomNumber1 + ".png";
+  return diceImage;
+}
+//Function determines winer.
+function result() {
+  //The variable stores the number of the dice image from player 1.
+  var player1 = Array.from(imgDom[0].getAttribute('src'));
+  player1 = player1[11];
+  //The variable stores the number of the dice image from player 2.
+  var player2 = Array.from(imgDom[1].getAttribute('src'));
+  player2 = player2[11];
+
+  //Game result flow control
+  if (player1 > player2) {
+    document.querySelector(".result").innerHTML = "Player1 won";
+  } else if (player1 === player2) {
+    document.querySelector(".result").innerHTML = "Draw";
+  } else {
+    document.querySelector(".result").innerHTML = "Player2 won";
+  }
+}
+
+//Function Set img by clicking button Start.
+function setImage() {
+
+  setTimeout(() => {
+    const img1 = imgDom[0].setAttribute("src", randomDiceImage());
+  }, 400);
+  setTimeout(() => {
+    const img2 = imgDom[1].setAttribute("src", randomDiceImage());
+  }, 900);
+
+  setTimeout(() => {
+    result();
+  }, 2000);
+
+}
