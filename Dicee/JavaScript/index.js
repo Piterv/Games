2d40f895ca5bb4d373335aa814df8af2a51dc5d8
Peter <peter.zgurovets@gmail.com>
2022-04-18 14:25:01 -0700

    Add files via upload
    Add function startGame();

--- Dicee/JavaScript/index.js
+++ Dicee/JavaScript/index.js
@@ -1,33 +1,49 @@
 const imgDom = document.querySelectorAll(".container img");
 const heading = document.querySelector(".result");
+//
 const scorePlayer1Dom = document.querySelector(".score-Player1");
 const scorePlayer2Dom = document.querySelector(".score-Player2");
+//
+const player1 = document.querySelector('.player1-name');
+const player2 = document.querySelector('.player2-name');
 // Default game score.
 var scorePlayer1 = 0;
 var scorePlayer2 = 0;
-
-// Add event listener to the button Add name.
+// Store player name.
 var player1Name;
 var player2Name;
-
-const userNameButtons = document.querySelectorAll('button.player').forEach(buttonUserName => {
+//Add event listener for all buttons.
+const userNameButtons = document.querySelectorAll('button').forEach(buttonUserName => {
   buttonUserName.addEventListener('click', event => {
-    console.log(event.target.name);
+    startGame(event);
 
-    if (event.target.name == 'button1') {
-      player1Name = prompt("type your name");
-      const hiddenButtonPlayer1 = event.target.hidden = true;
-      const showPlayer1Name = document.querySelector('.player1-name').innerHTML = player1Name;
-
-    } else {
-      player2Name = prompt("type your name");
-      const hiddenButtonPlayer2 = event.target.hidden = true;
-      const showPlayer2Name = document.querySelector('.player2-name').innerHTML = player2Name;
-
-    }
   });
 });
 
+// Start game.
+function startGame(event) {
+
+  if (event.target.name === 'button1' || event.target.name === 'button2') {
+    setName(event);
+  } else if (event.target.name === 'start') {
+    if (scorePlayer1 === 0 && scorePlayer2 === 0) {
+      setName(event);
+      setTimeout(() => {
+        setImage()
+      }, 500);
+      setTimeout(() => {
+        gameResult();
+      }, 1500);
+    } else {
+      setTimeout(() => {
+        setImage();
+      }, 500);
+      setTimeout(() => {
+        gameResult();
+      }, 1500);
+    }
+  }
+}
 //The Function creates and return one random image.
 function randomDiceImage() {
   var randomNumber1 = Math.floor((Math.random() * 6) + 1);
@@ -43,10 +59,6 @@ function setImage() {
     const img2 = imgDom[1].setAttribute("src", randomDiceImage());
   }, 700);
 
-  setTimeout(() => {
-    gameResult();
-  }, 1000);
-
 }
 //Function determines winer.
 function gameResult() {
@@ -57,7 +69,7 @@ function gameResult() {
   var player2 = Array.from(imgDom[1].getAttribute('src'));
   player2 = player2[11];
 
-  //Game result flow control
+     //Game result flow control
   if (player1 > player2) {
     scorePlayer1++;
     document.querySelector(".result").innerHTML = "Player1 won";
@@ -67,16 +79,35 @@ function gameResult() {
     scorePlayer2++;
     document.querySelector(".result").innerHTML = "Player2 won";
   }
-
   const setScorePlayer1 = document.querySelector(".score-Player1").innerHTML = scorePlayer1;
   const setScorePlayer2 = document.querySelector(".score-Player2").innerHTML = scorePlayer2;
 }
 
-// Player name.
+//Set player's name.
+function setName(event) {
 
-function userName(event) {
-
-  const userName = event.name;
-
-  console.log(userName);
+  if (event.target.name == 'button1') {
+    player1Name = prompt("type your name");
+    if (player1Name === null) {
+      event.target.hidden = true;
+      player1.innerHTML = "Player1";
+    } else {
+      event.target.hidden = true;
+      player1.innerHTML = player1Name;
+    }
+  } else if (event.target.name == 'button2') {
+    player2Name = prompt("type your name");
+    if (player2Name === null) {
+      event.target.hidden = true;
+      player2.innerHTML = "Player2";
+    } else {
+      event.target.hidden = true;
+      player2.innerHTML = player2Name;
+    }
+  } else {
+    document.querySelector('.player1-name button').hidden = true;
+    player1.innerHTML = "Player1";
+    document.querySelector('.player2-name button').hidden = true;
+    player2.innerHTML = "Player2";
+  }
 }
