38934743fccee6df315449ec962a9ab51c099b73
Peter <peter.zgurovets@gmail.com>
2022-04-19 19:39:39 -0700

    Add files via upload
    Added button and function to restart the game.

--- Dicee/JavaScript/index.js
+++ Dicee/JavaScript/index.js
@@ -1,32 +1,33 @@
+//
+var buttonName1 = document.querySelector('.player1-name button');
+//
 const imgDom = document.querySelectorAll(".container img");
 const heading = document.querySelector(".result");
 //
-const scorePlayer1Dom = document.querySelector(".score-Player1");
-const scorePlayer2Dom = document.querySelector(".score-Player2");
+const playerOneScoreDom = document.querySelector(".score-Player1");
+const playerTwoScoreDom = document.querySelector(".score-Player2");
 //
 const player1 = document.querySelector('.player1-name');
 const player2 = document.querySelector('.player2-name');
 // Default game score.
-var scorePlayer1 = 0;
-var scorePlayer2 = 0;
+var playerOneScore = 0;
+var playerTwoScore = 0;
 // Store player name.
-var player1Name;
-var player2Name;
+var playerOneName;
+var playerTwoName;
+
 //Add event listener for all buttons.
 const userNameButtons = document.querySelectorAll('button').forEach(buttonUserName => {
   buttonUserName.addEventListener('click', event => {
     startGame(event);
-
   });
 });
-
-// Start game.
+// Function start game.
 function startGame(event) {
-
   if (event.target.name === 'button1' || event.target.name === 'button2') {
     setName(event);
   } else if (event.target.name === 'start') {
-    if (scorePlayer1 === 0 && scorePlayer2 === 0) {
+    if (playerOneScore === 0 && playerTwoScore === 0) {
       setName(event);
       setTimeout(() => {
         setImage()
@@ -37,77 +38,125 @@ function startGame(event) {
     } else {
       setTimeout(() => {
         setImage();
-      }, 500);
+      }, 100);
       setTimeout(() => {
         gameResult();
-      }, 1500);
+      }, 1000);
     }
+  } else {
+
+    StartOver(event);
   }
 }
 //The Function creates and return one random image.
 function randomDiceImage() {
-  var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-  var diceImage = "images/dice" + randomNumber1 + ".png";
-  return diceImage;
+  const randomNumber = Math.floor((Math.random() * 6) + 1);
+  const randomImage = "images/dice" + randomNumber + ".png";
+  return randomImage;
 }
 //Function rundomly set imiges by clicking button Start.
 function setImage() {
   setTimeout(() => {
-    const img1 = imgDom[0].setAttribute("src", randomDiceImage());
+    const imgForPlayerOne = imgDom[0].setAttribute("src", randomDiceImage());
   }, 400);
   setTimeout(() => {
-    const img2 = imgDom[1].setAttribute("src", randomDiceImage());
+    const imgForPlayerTwo = imgDom[1].setAttribute("src", randomDiceImage());
   }, 700);
-
 }
 //Function determines winer.
 function gameResult() {
-  //The variable stores the number of the dice image from player 1.
-  var player1 = Array.from(imgDom[0].getAttribute('src'));
-  player1 = player1[11];
-  //The variable stores the number of the dice image from player 2.
-  var player2 = Array.from(imgDom[1].getAttribute('src'));
-  player2 = player2[11];
+  //The number of the dice image from player 1.
+  var player1 = Array.from(imgDom[0].getAttribute('src'))[11];
+  //The number of the dice image from player 2.
+  var player2 = Array.from(imgDom[1].getAttribute('src'))[11];
 
-     //Game result flow control
   if (player1 > player2) {
-    scorePlayer1++;
-    document.querySelector(".result").innerHTML = "Player1 won";
+    playerOneScore++;
+    document.querySelector(".result").textContent = "Player1 won";
   } else if (player1 === player2) {
-    document.querySelector(".result").innerHTML = "Draw";
+    document.querySelector(".result").textContent = "Draw";
   } else {
-    scorePlayer2++;
-    document.querySelector(".result").innerHTML = "Player2 won";
+    playerTwoScore++;
+    document.querySelector(".result").textContent = "Player2 won";
   }
-  const setScorePlayer1 = document.querySelector(".score-Player1").innerHTML = scorePlayer1;
-  const setScorePlayer2 = document.querySelector(".score-Player2").innerHTML = scorePlayer2;
+  playerOneScoreDom.textContent = playerOneScore;
+  playerTwoScoreDom.textContent = playerTwoScore;
 }
 
 //Set player's name.
 function setName(event) {
 
+  const name1Type = typeof playerOneName;
+  const name2Type = typeof playerTwoName;
+
   if (event.target.name == 'button1') {
-    player1Name = prompt("type your name");
-    if (player1Name === null) {
+    playerOneName = prompt("type your name");
+    if (playerOneName === "") {
+        buttonToggle(event);
+
+      playerOneName = player1.textContent = "Player1";
+    } else if (playerOneName === null) {
       event.target.hidden = true;
-      player1.innerHTML = "Player1";
+      playerOneName = player1.textContent = "Player1";
     } else {
       event.target.hidden = true;
-      player1.innerHTML = player1Name;
+      player1.textContent = playerOneName;
     }
   } else if (event.target.name == 'button2') {
-    player2Name = prompt("type your name");
-    if (player2Name === null) {
+    playerTwoName = prompt("type your name");
+    if (playerTwoName === "") {
       event.target.hidden = true;
-      player2.innerHTML = "Player2";
+      playerTwoName = player2.textContent = "Player2";
+    } else if (playerTwoName === null) {
+      event.target.hidden = true;
+      playerTwoName = player2.textContent = "Player2";
     } else {
       event.target.hidden = true;
-      player2.innerHTML = player2Name;
+      player2.textContent = playerTwoName;
     }
-  } else {
-    document.querySelector('.player1-name button').hidden = true;
-    player1.innerHTML = "Player1";
-    document.querySelector('.player2-name button').hidden = true;
-    player2.innerHTML = "Player2";
+  } else if (event.target.name == 'start') {
+    if (playerOneName === undefined && playerTwoName === undefined) {
+      document.querySelector('.player1-name button').hidden = true;
+      playerOneName = player1.textContent = "Player1";
+      document.querySelector('.player2-name button').hidden = true;
+      playerTwoName = player2.textContent = "Player2";
+    } else if (name1Type === 'string' && name2Type === 'undefined') {
+      document.querySelector('.player2-name button').hidden = true;
+      playerTwoName = player2.textContent = "Player2";
+    } else if (name1Type === 'undefined' && name2Type === 'string') {
+      document.querySelector('.player1-name button').hidden = true;
+      playerOneName = player1.textContent = "Player1";
+    }
   }
 }
+//
+function buttonToggle(event) {
+  console.log(buttonName1);
+if (true) {
+  buttonName1.toggleAttribute('hidden');
+}else {
+  buttonName1.toggleAttribute('hidden');
+}
+
+
+}
+
+//Function reset all parameters to default.
+function StartOver(event) {
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  playerOneName = '';
+  playerTwoName = '';
+
+  player1.textContent = '';
+  player2.textContent = '';
+
+  document.querySelector(".result").textContent = "Refresh Me";
+  playerOneScoreDom.textContent = playerOneScore;
+  playerTwoScoreDom.textContent = playerTwoScore;
+
+  buttonToggle(event);
+
+
+}
+//Toggle between hiding and showing an element.
