03a04e1aaece0c2e71c39ba33a0fc516fb85b401
Peter <peter.zgurovets@gmail.com>
2022-05-04 19:51:40 -0700

    Add files via upload

--- The Simon Game/JavaScript/index.js
+++ The Simon Game/JavaScript/index.js
@@ -1,19 +1,54 @@
-const color = ['green', 'red', 'yellow', 'blue'];
+const color = ['green', 'red', 'yellow', 'blue', 'wrong'];
 const mySounds = {
   green: "sounds/green.mp3",
   red: "sounds/red.mp3",
   yellow: "sounds/yellow.mp3",
-  blue: "sounds/blue.mp3"
+  blue: "sounds/blue.mp3",
+  wrong: "sounds/wrong.mp3"
 }
 let userSequence = [];
 let gameSequence = [];
+let gameLevel = [];
 
-$('.btn').on('click', (e) => {
-  userSequence.push(e.target.id);
-  console.log(userSequence);
-  pressedButton(e);
-  rundomColor();
+// Listen to keydown
+$(document).on('keydown', (e) => {
+  if (gameSequence.length === 0) {
+    $('h1').text('Level:' + gameLevel.length);
+    gameColorSequence();
+  }
 });
+//Listen to click.
+$('.btn').on('click', (e) => {
+
+  userSequence.push(e.target.id);
+  let i = 1 - userSequence.length;
+
+  if (userSequence[i] === gameSequence[i]) {
+    pressedButton(e.target.id);
+    playSound(e.target.id);
+
+    if (userSequence.length === gameSequence.length) {
+      gameColorSequence();
+      userSequence = [];
+    }
+  } else {
+    playSound('wrong');
+    userSequence = [];
+    gameSequence = [];
+    gameLevel = [];
+  }
+
+});
+//Function display color sequence from game Sequence array.\
+function gameColorSequence() {
+  rundomColor();
+  console.log(gameSequence);
+  for (let i = 1; i <= gameSequence.length; i++) {
+    setTimeout(function() {
+      pressedButton(gameSequence[i - 1]);
+    }, 1200 * i);
+  }
+}
 //Function play sound.
 function playSound(sound) {
   const audio = new Audio(mySounds[sound]);
@@ -23,12 +58,10 @@ function playSound(sound) {
 function rundomColor() {
   let rundomNumber = Math.floor(Math.random() * 4);
   gameSequence.push(color[rundomNumber]);
-  console.log(gameSequence);
 }
 //Add clase that show presed button.
 function pressedButton(e) {
-  playSound(e.target.id);
-  $(e.target).addClass('pressed').delay(200).queue(function(next) {
+  $('#' + e).addClass('pressed').delay(250).queue(function(next) {
     $(this).removeClass('pressed');
     next();
   });
