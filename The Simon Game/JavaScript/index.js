c25f35e63ad95b3a3402981bcb0c4f06fb8cd343
Peter <peter.zgurovets@gmail.com>
2022-05-05 09:16:53 -0700

    Add files via upload

--- The Simon Game/JavaScript/index.js
+++ The Simon Game/JavaScript/index.js
@@ -1,69 +1,70 @@
-const color = ['green', 'red', 'yellow', 'blue', 'wrong'];
-const mySounds = {
-  green: "sounds/green.mp3",
-  red: "sounds/red.mp3",
-  yellow: "sounds/yellow.mp3",
-  blue: "sounds/blue.mp3",
-  wrong: "sounds/wrong.mp3"
-}
-let userSequence = [];
-let gameSequence = [];
+const color = ['green', 'red', 'yellow', 'blue'];
+
+let gamePattern = [];
+let userClickedPattern = [];
+
+var started = false;
 let gameLevel = 0;
 
 // Listen to keydown
 $(document).on('keydown', (e) => {
-  if (gameSequence.length === 0) {
+  if (!started) {
     $('h1').text('Level:' + gameLevel);
     gameColorSequence();
+    started = true;
   }
 });
 //Listen to click.
 $('.btn').on('click', (e) => {
 
-  userSequence.push(e.target.id);
-  let i = 1 - userSequence.length;
+  gamePattern.push(e.target.id);
 
-  if (userSequence[i] === gameSequence[i]) {
+  let i = gamePattern.length - 1;
+
+  if (gamePattern[i] === userClickedPattern[i]) {
     pressedButton(e.target.id);
     playSound(e.target.id);
 
-    if (userSequence.length === gameSequence.length) {
+    if (gamePattern.length === userClickedPattern.length) {
       gameLevel++;
       setTimeout(function() {
         $('h1').text('Level:' + gameLevel);
-      }, 800);
+      }, 700);
       setTimeout(function() {
         gameColorSequence();
-      }, 600);
-      userSequence = [];
+      }, 500);
+      gamePattern = [];
     }
   } else {
     playSound('wrong');
-    userSequence = [];
-    gameSequence = [];
-    gameLevel = ;
-    $('h1').text('Press A Key to Start');
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+
   }
 });
 //The function displays a sequence of colors from the Sequence game array.
 function gameColorSequence() {
   rundomColor();
-  console.log(gameSequence);
-  for (let i = 1; i <= gameSequence.length; i++) {
+  for (let i = 1; i <= userClickedPattern.length; i++) {
     setTimeout(function() {
-      pressedButton(gameSequence[i - 1]);
+      pressedButton(userClickedPattern[i - 1]);
     }, 1200 * i);
   }
 }
 //Sound playback function.
 function playSound(sound) {
-  const audio = new Audio(mySounds[sound]);
+  const audio = new Audio("sounds/" + sound + ".mp3"); //
   audio.play();
 }
 // Sets random color.
 function rundomColor() {
   let rundomNumber = Math.floor(Math.random() * 4);
-  gameSequence.push(color[rundomNumber]);
+  userClickedPattern.push(color[rundomNumber]);
 }
 //Adds class that show pressed button.
 function pressedButton(e) {
@@ -71,4 +72,12 @@ function pressedButton(e) {
     $(this).removeClass('pressed');
     next();
   });
+}
+// Function Start set game information to the begining.
+function startOver() {
+  gamePattern = [];
+  userClickedPattern = [];
+  gameLevel = 0;
+  started = false;
+  $('h1').text('Press A Key to Start');
 }
